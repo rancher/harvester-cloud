7f0213a2080887f55e8e6f144c90d4377c9dd901
saurabhsadhalesuse <saurabh.sadhale@suse.com>
2026-03-24 20:33:41 +0530

    Remove ssh-keygen command from config.sh

--- modules/harvester/kiwi-os-image-builder/config.sh
+++ modules/harvester/kiwi-os-image-builder/config.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -ex
 
-ssh-keygen -A 
-
 systemctl enable sshd
 systemctl enable libvirtd
+systemctl enable systemd-networkd.service
 systemctl enable cloud-init
 systemctl enable cloud-init-local
 systemctl enable cloud-config
